115f54ab74594893acd8eecb45b4bcbaf3eacb31
ReFeXe <129334095+ReFeXe@users.noreply.github.com>
2023-04-03 01:38:04 +0300

    Update app.c

--- applications/app.c
+++ applications/app.c
@@ -132,10 +132,12 @@ void app_set_configuration(app_configuration *conf) {
 			break;
 
 		case APP_PAS:
+			app_uartcomm_start(UART_PORT_COMM_HEADER);
 			app_pas_start(true);
 			break;
 
 		case APP_ADC_PAS:
+			app_uartcomm_start(UART_PORT_COMM_HEADER);
 			app_adc_start(false);
 			app_pas_start(false);
 			break;
