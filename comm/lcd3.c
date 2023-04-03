16c0c363683612cce085a8d637cb4a5970d030fe
ReFeXe <129334095+ReFeXe@users.noreply.github.com>
2023-04-03 07:44:58 +0300

    Update lcd3.c

--- comm/lcd3.c
+++ comm/lcd3.c
@@ -108,7 +108,7 @@ void lcd3_process_packet(unsigned char *data, unsigned int len,
 	sb[7] = 
 		((app_adc_get_decoded_level() > 0) ? MOVING_ANIMATION_THROTTLE : 0) |
 		(0) |
-		((lcd_pas_mode > 1) ? MOVING_ANIMATION_ASSIST : 0) |
+		((app_pas_get_pedal_rpm() > 0) ? MOVING_ANIMATION_ASSIST : 0) |
 		((app_adc_get_decoded_level2() > 0) ? MOVING_ANIMATION_BRAKE : 0);
 	
 	sb[8] = w;	//b8: power in 13 wt increments (48V version of the controller)
