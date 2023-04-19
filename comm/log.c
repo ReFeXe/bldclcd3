03a90ea1ef35b81086ef84d6b60a51db0e9a1deb
ReFeXe <129334095+ReFeXe@users.noreply.github.com>
2023-04-19 10:05:12 +0300

    Update log.c

--- comm/log.c
+++ comm/log.c
@@ -1,163 +1,160 @@
-#include "commands.h"
-#include "ch.h"
-#include "hal.h"
-#include "mc_interface.h"
-#include "stm32f4xx_conf.h"
-#include "buffer.h"
-#include "terminal.h"
-#include "hw.h"
-#include "app.h"
-#include "timeout.h"
-#include "utils_sys.h"
-#include "packet.h"
-#include "qmlui.h"
-#include "crc.h"
-#include "main.h"
-#include "conf_custom.h"
+/*
+	Copyright 2022 Benjamin Vedder	benjamin@vedder.se
 
-#include <math.h>
+	This file is part of the VESC firmware.
+
+	The VESC firmware is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The VESC firmware is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    */
+
 #include <string.h>
-#include <stdarg.h>
-#include <stdio.h>
+#include "log.h"
+#include "comm_can.h"
+#include "commands.h"
+#include "buffer.h"
+#include "datatypes.h"
+#include "mempools.h"
 
+void log_start(
+		int can_id,
+		int field_num,
+		float rate_hz,
+		bool append_time,
+		bool append_gnss,
+		bool append_gnss_time) {
 
-extern void app_uartcomm_send_raw_packet(unsigned char *data, unsigned int len, UART_PORT port_number);
+	int32_t ind = 0;
+	uint8_t buffer[20];
 
-#define LCD3_REPLY_PACKET_SIZE	12
+	buffer[ind++] = COMM_LOG_START;
+	buffer_append_int16(buffer, field_num, &ind);
+	buffer_append_float32_auto(buffer, rate_hz, &ind);
+	buffer[ind++] = append_time;
+	buffer[ind++] = append_gnss;
+	buffer[ind++] = append_gnss_time;
 
-#define MOVING_ANIMATION_THROTTLE	(1 << 1)
-#define MOVING_ANIMATION_CRUISE		(1 << 3)
-#define MOVING_ANIMATION_ASSIST		(1 << 4)
-#define MOVING_ANIMATION_BRAKE		(1 << 5)
-
-void lcd3_process_packet(unsigned char *data, unsigned int len,
-		void(*reply_func)(unsigned char *data, unsigned int len))
-{
-	volatile mc_configuration *mcconf = (volatile mc_configuration*) mc_interface_get_configuration();
-
-	(void)len;
-	(void)reply_func;
-	
-	uint8_t lcd_pas_mode = data[1];
-	uint8_t fixed_throttle_level = data[4] & (1 << 4);
-	
-	float current_scale;
-	
-	if (lcd_pas_mode == 1)
-		current_scale = 0.1;
-	else if (lcd_pas_mode == 2)
-		current_scale = 0.2;
-	else if (lcd_pas_mode == 3)
-		current_scale = 0.35;
-	else if (lcd_pas_mode == 4)
-		current_scale = 0.65;
-	else if (lcd_pas_mode == 5)
-		current_scale = 1;
-		
-	
-	if(fixed_throttle_level == 0) {
-		mcconf->l_current_max_scale = 1.0;
-		app_pas_set_current_sub_scaling(current_scale);
+	if (can_id >= 0 && can_id < 255) {
+		comm_can_send_buffer(can_id, buffer, ind, 0);
 	} else {
-		mcconf->l_current_max_scale = current_scale;
+		commands_send_packet(buffer, ind);
 	}
-	
-	if(current_scale == 0.0) {
-		mcconf->l_current_max_scale = current_scale;
-	}
-	
-	
-	uint8_t sb[LCD3_REPLY_PACKET_SIZE];
-	
-	uint16_t ms = (uint16_t)(mc_interface_get_speed() * 3600.0 / 1000.0 * 100.0 );
-	
-	uint8_t batteryLevel;
-	uint8_t batFlashing = 0;
-
-	float l = mc_interface_get_battery_level(NULL);
-	
-	
-	
-	if (l > 0.7)
-		batteryLevel = 4;
-	else if (l > 0.4)
-		batteryLevel = 3;
-	else if (l > 0.2)
-		batteryLevel = 2;
-	else if (l > 0.1)
-		batteryLevel = 1;
-	else
-	{
-		batteryLevel = 0;
-		if (l <= 0)
-			batFlashing = 1;
-	}
-	
-	
-	float w = (float)GET_INPUT_VOLTAGE() * mc_interface_read_reset_avg_input_current() / 12;
-	if (w < 0)
-		w = 0;
-	if (w > 255)
-		w = 255;
-	
-	sb[0] = 0x41;
-	
-	//b1: battery level:
-	// bit 0: border flashing,
-	// bit 1: animated charging,
-	// bit 3-5: level, (0-4)
-	sb[1] = (batteryLevel << 2) | batFlashing;
-	
-	sb[2] = 0x30;
-	
-	sb[3] = (uint8_t)(ms & 0x00ff);	//b3, b4: speed, wheel rotation period, ms; period(ms)=B3*256+B4;
-	sb[4] = (uint8_t)((ms >> 8 ) & 0x00ff);
-	
-	sb[5] = 0;	//b5: B5 error info display: 0x20: "0info", 0x21: "6info", 0x22: "1info", 0x23: "2info", 0x24: "3info", 0x25: "0info", 0x26: "4info", 0x28: "0info"
-	sb[6] = 0;
-	
-	//b7: moving animation ()
-	// bit 0: -
-	// bit 1: throttle
-	// bit 2: -
-	// bit 3: cruise
-	// bit 4: assist
-	// bit 5: brake
-	// bit 6: -
-	// bit 7: -
-	sb[7] = 
-		((app_adc_get_decoded_level() > 0) ? MOVING_ANIMATION_THROTTLE : 0) |
-		(0) |
-		((app_pas_get_pedal_rpm() > 0) ? MOVING_ANIMATION_ASSIST : 0) |
-		((app_adc_get_decoded_level2() > 0) ? MOVING_ANIMATION_BRAKE : 0);
-	
-	sb[8] = w;	//b8: power in 13 wt increments (48V version of the controller)
-	//sb[9] = (int8_t)(mc_interface_temp_motor_filtered() - 15.0f);	//b9: motor temperature +15
-	sb[9] = (int8_t)(mc_interface_temp_fet_filtered() - 15.0f);	//b9: motor temperature +15
-	sb[10] = 0;	//
-	sb[11] = 0;	//
-	
-	uint8_t crc = 0;
-	for (int n = 1; n < LCD3_REPLY_PACKET_SIZE; n++)
-		crc ^= sb[n];
-	
-	sb[6] = crc;
-	
-	app_uartcomm_send_raw_packet(sb,len,UART_PORT_COMM_HEADER);
 }
 
-#define LCD3_RX_PACKET_SIZE	13
-static uint8_t buffer[LCD3_RX_PACKET_SIZE];
+void log_stop(int can_id) {
+	int32_t ind = 0;
+	uint8_t buffer[10];
+	buffer[ind++] = COMM_LOG_STOP;
 
-void lcd3_process_byte(uint8_t rx_data, PACKET_STATE_t *state)
-{
-	(void)state;
-	
-	memmove(buffer, &buffer[1], LCD3_RX_PACKET_SIZE - 1);
-	buffer[LCD3_RX_PACKET_SIZE - 1] = rx_data;
-	
-	if (buffer[12] == 0x0e)
-	{
-		lcd3_process_packet(buffer, LCD3_RX_PACKET_SIZE, UART_PORT_COMM_HEADER);
+	if (can_id >= 0 && can_id < 255) {
+		comm_can_send_buffer(can_id, buffer, ind, 0);
+	} else {
+		commands_send_packet(buffer, ind);
 	}
+}
+
+void log_config_field(
+		int can_id,
+		int field_ind,
+		char *key,
+		char *name,
+		char *unit,
+		int precision,
+		bool is_relative,
+		bool is_timestamp) {
+
+	size_t key_len = strlen(key);
+	size_t name_len = strlen(name);
+	size_t unit_len = strlen(unit);
+
+	if ((key_len + name_len + unit_len) > 400) {
+		return;
+	}
+
+	int32_t ind = 0;
+	uint8_t *buffer = mempools_get_packet_buffer();
+
+	buffer[ind++] = COMM_LOG_CONFIG_FIELD;
+	buffer_append_int16(buffer, field_ind, &ind);
+	strcpy((char*)(buffer + ind), key); ind += key_len + 1;
+	strcpy((char*)(buffer + ind), name); ind += name_len + 1;
+	strcpy((char*)(buffer + ind), unit); ind += unit_len + 1;
+	buffer[ind++] = precision;
+	buffer[ind++] = is_relative;
+	buffer[ind++] = is_timestamp;
+
+	if (can_id >= 0 && can_id < 255) {
+		comm_can_send_buffer(can_id, buffer, ind, 0);
+	} else {
+		commands_send_packet(buffer, ind);
+	}
+
+	mempools_free_packet_buffer(buffer);
+}
+
+void log_send_samples_f32(
+		int can_id,
+		int field_start,
+		float *samples,
+		int sample_num) {
+
+	if (sample_num > 100) {
+		return;
+	}
+
+	int32_t ind = 0;
+	uint8_t *buffer = mempools_get_packet_buffer();
+
+	buffer[ind++] = COMM_LOG_DATA_F32;
+	buffer_append_int16(buffer, field_start, &ind);
+
+	for (int i = 0;i < sample_num;i++) {
+		buffer_append_float32_auto(buffer, samples[i], &ind);
+	}
+
+	if (can_id >= 0 && can_id < 255) {
+		comm_can_send_buffer(can_id, buffer, ind, 0);
+	} else {
+		commands_send_packet(buffer, ind);
+	}
+
+	mempools_free_packet_buffer(buffer);
+}
+
+void log_send_samples_f64(
+		int can_id,
+		int field_start,
+		double *samples,
+		int sample_num) {
+
+	if (sample_num > 50) {
+		return;
+	}
+
+	int32_t ind = 0;
+	uint8_t *buffer = mempools_get_packet_buffer();
+
+	buffer[ind++] = COMM_LOG_DATA_F64;
+	buffer_append_int16(buffer, field_start, &ind);
+
+	for (int i = 0;i < sample_num;i++) {
+		buffer_append_float64_auto(buffer, samples[i], &ind);
+	}
+
+	if (can_id >= 0 && can_id < 255) {
+		comm_can_send_buffer(can_id, buffer, ind, 0);
+	} else {
+		commands_send_packet(buffer, ind);
+	}
+
+	mempools_free_packet_buffer(buffer);
 }
