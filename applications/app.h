480e67c875eab318efedda1fc3d229a1f3d0fd4d
ReFeXe <129334095+ReFeXe@users.noreply.github.com>
2023-04-21 08:29:24 +0300

    Update app.h

--- applications/app.h
+++ applications/app.h
@@ -97,7 +97,7 @@ void app_pas_configure(pas_config *conf);
 float app_pas_get_current_target_rel(void);
 float app_pas_get_pedal_rpm(void);
 void app_pas_set_current_sub_scaling(float current_sub_scaling);
-void app_pas_set_one_magnet(bool pas_one_magnet);
+void app_pas_set_one_magnet(bool use_one_magnet);
 
 // Custom apps
 void app_custom_start(void);
