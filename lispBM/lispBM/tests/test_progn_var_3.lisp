99b20b1fed02089acf9915d56c46b0cc1c378da0
ReFeXe <alexpti@ya.ru>
2023-04-23 12:53:01 +0300

    master nut suport

--- lispBM/lispBM/tests/test_progn_var_3.lisp
+++
@@ -1,9 +0,0 @@
-
-
-(defun f ()
-  (progn
-    (var a 10)
-    (var b 20)
-    (+ a b)))
-
-(= (f) 30)
--- lispBM/lispBM/tests/test_progn_var_4.lisp
+++
@@ -1,8 +0,0 @@
-
-(defun f ()
-  (progn
-    (var a 10)
-    (var b (+ a 10))
-    (+ a b)))
-
-(= (f) 30)
